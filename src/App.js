eb33da9867ef40ffaca71a79ad6b4eb7d040cc6c
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-23 12:40:51 +0600

    ApolloProvider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import {
-  ApolloClient, ApolloProvider, from, HttpLink, InMemoryCache
+  ApolloClient,
+  ApolloProvider,
+  from,
+  HttpLink,
+  InMemoryCache
 } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
 import "./App.css";
-const ddd = { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from,onError };
+const ddd = {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+  onError,
+};
 
-const link = from ([
- errorLink,
- new HttpLink({
-  uri: "http://localhost:5000/graphql"
- })
-])
+const errorLink = onError(({ graphqlErrors, networkErrors }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`GraphQl error ${message} with${location} ${path}`);
+    });
+  }
+});
 
-const client  = new ApolloClient({
-  cache: {},
+const link = from([
+  errorLink,
+  new HttpLink({
+    uri: "http://localhost:5000/graphql",
+  }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
   link: link,
-})
+});
 
 function App() {
-  return <div>dsdsdfsfdf</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
