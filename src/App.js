6d18c6406601e5562681cc42ec76ed3820c62605
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-23 12:29:03 +0600

    client

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import {
+  ApolloClient, ApolloProvider, from, HttpLink, InMemoryCache
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import "./App.css";
+const ddd = { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from,onError };
+
+const link = from ([
+ errorLink,
+ new HttpLink({
+  uri: "http://localhost:5000/graphql"
+ })
+])
+
+const client  = new ApolloClient({
+  cache: {},
+  link: link,
+})
 
 function App() {
-  return (
-    <div>
-      dsdsdfsfdf
-    </div>
-  );
+  return <div>dsdsdfsfdf</div>;
 }
 
 export default App;
