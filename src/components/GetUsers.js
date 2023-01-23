e038a498a8bc3549e90c631caacc2eb42cb1d5bd
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-23 13:10:02 +0600

    data showed

--- src/components/GetUsers.js
+++ src/components/GetUsers.js
@@ -1,15 +1,28 @@
 import { useQuery } from "@apollo/client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { LOAD_USERS } from "../GraphQL/Queries";
 
 export default function GetUsers() {
   const { error, loading, data } = useQuery(LOAD_USERS);
+  const [users, setUsers] = useState([]);
+
+  console.log(users);
   useEffect(() => {
-    console.log(data);
+    if (data) {
+      setUsers(data.getAllUsers);
+    }
   }, [data]);
   return (
     <div>
-      <div>GetUsers</div>
+      <div>
+        {
+            users?.slice(0,200).map((u)=>(
+                <div key={u.id}>
+                    {u.email}
+                </div>
+            ))
+        }
+      </div>
     </div>
   );
 }
